9566d5f3b48d949d8627d9ad2210b71ac0b56ec7
Danny Festor <danny.festor@gmail.com>
2020-09-24 10:19:54 +0900

    Nuxt.js Scaffold

---
+++ components/Service/ServiceOverview.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full flex flex-row justify-between">
+    <ServiceView text="Test Text">Test Slot</ServiceView>
+    <ServiceView text="Test Text">Test Slot</ServiceView>
+    <ServiceView text="Test Text">Test Slot</ServiceView>
+  </div>
+</template>
---
+++ components/Service/ServiceView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="service">
+    {{ text }}
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.service {
+  border: 1px solid black;
+}
+</style>
+
+<script>
+export default {
+  props: ["text"],
+};
+</script>
