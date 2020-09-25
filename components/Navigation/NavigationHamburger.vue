d3e1495f321fe90ea8470e1c9b9a758574a661b7
Danny Festor <danny.festor@gmail.com>
2020-09-25 15:26:34 +0900

    Make Navigation Hamburger

---
+++ components/Navigation/NavigationHamburger.vue
@@ -0,0 +1,57 @@
+<template>
+  <div
+    class="hamburger lg:hidden ml-auto mr-3"
+    :class="{ active: isActive }"
+    @click="hamburgerClicked"
+  >
+    <div></div>
+  </div>
+</template>
+
+<style>
+.hamburger {
+  border: 2px solid white;
+  border-radius: 2px;
+  width: 50px;
+  padding: 0 5px;
+}
+
+.hamburger::before,
+.hamburger::after,
+.hamburger div {
+  background-color: white;
+  border-radius: 3px;
+  content: "";
+  display: block;
+  height: 5px;
+  margin: 7px 0;
+  transition: all 0.2s ease-in-out;
+}
+
+.hamburger.active::before {
+  transform: translateY(12px) translateX(1px) rotate(135deg);
+}
+.hamburger.active::after {
+  transform: translateY(-12px) translateX(1px) rotate(-135deg);
+}
+.hamburger.active div {
+  opacity: 0;
+}
+</style>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+  methods: {
+    hamburgerClicked() {
+      this.isActive = !this.isActive;
+      this.$emit("hamburgerClicked");
+    },
+  },
+});
+</script>
--- components/Navigation/NavigationSocial.vue
+++ components/Navigation/NavigationSocial.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="h-16 px-3 inline-flex items-center font-semibold">
-    <img src="~assets/svgs/github.svg" class="h-8" style="fill: white" />
-  </div>
+  <a
+    :href="target"
+    target="_blank"
+    class="h-16 px-4 lg:px-1 inline-flex items-center hover:bg-blue-800"
+  >
+    <svg
+      role="img"
+      height="32"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>{{ title }}</title>
+      <slot></slot>
+    </svg>
+  </a>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: ["title", "target"],
+});
+</script>
