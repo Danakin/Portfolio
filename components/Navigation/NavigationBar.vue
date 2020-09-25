0e27b8731892d72119748fb795319fa85f6fbd63
Danny Festor <danny.festor@gmail.com>
2020-09-25 11:19:52 +0900

    Fix SVG

--- components/Navigation/NavigationBar.vue
+++ components/Navigation/NavigationBar.vue
@@ -1,152 +1,66 @@
 <template>
-  <nav class="flex items-center bg-gray-800 p-3 flex-wrap fixed">
-    <a href="#">
+  <nav class="flex items-center bg-gray-800 flex-wrap fixed">
+    <a class="pl-3" href="#">
       <svg
-        v-if="width > 300"
+        v-if="width > 640"
         width="98.286"
         height="27.883"
         version="1.1"
         viewBox="0 0 26.005 7.3774"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:cc="http://creativecommons.org/ns#"
-        xmlns:dc="http://purl.org/dc/elements/1.1/"
-        xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
       >
-        <metadata>
-          <rdf:RDF>
-            <cc:Work rdf:about="">
-              <dc:format>image/svg+xml</dc:format>
-              <dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
-              <dc:title />
-            </cc:Work>
-          </rdf:RDF>
-        </metadata>
-        <g transform="translate(-.3163 -.80459)">
-          <text
-            x="-0.21428314"
-            y="7.8902383"
-            style="
-              fill: #fff;
-              font-family: FranklinGothic-Medium, 'Franklin Gothic Medium';
-              font-size: 7.4038px;
-              font-weight: 500;
-              stroke-width: 0.33978;
-            "
-          >
-            <tspan style="stroke-width: 0.33978" xml:space="preserve">BR</tspan>
-          </text>
-          <path
-            class="cls-2"
-            d="m10.688 2.5585-1.1134-0.056329a0.51088 0.51088 0 0 1-0.52145-0.47213v-0.67295a0.47213 0.47213 0 0 1 0.53554-0.41576l1.0922 0.056329a0.51088 0.51088 0 0 1 0.51794 0.46861v0.70466a0.44746 0.44746 0 0 1-0.51089 0.38758z"
-            style="
-              fill: none;
-              stroke-miterlimit: 10;
-              stroke-width: 0.26425px;
-              stroke: #fff;
-            "
-          />
-          <path
-            class="cls-2"
-            d="m11.135 1.7799a1.4798 1.4798 0 0 1 1.0218 0.028231c0.1092 0.042347 0.45451 0.18674 0.49326 0.29596 0.03875 0.1092-0.06698 0.20788-0.1092 0.28892a0.42985 0.42985 0 0 0 0.27483 0.5743l1.0218 0.42633a0.53555 0.53555 0 0 0 0.68353-0.20083l0.303-0.60954a0.45099 0.45099 0 0 0-0.28539-0.59897l-1.0006-0.41576a0.53202 0.53202 0 0 0-0.68353 0.1973l-0.17616 0.35234"
-            style="
-              fill: none;
-              stroke-miterlimit: 10;
-              stroke-width: 0.26425px;
-              stroke: #fff;
-            "
-          />
-          <path
-            class="cls-3"
-            d="m13.453 3.8058-0.47212 2.3466a1.3706 1.3706 0 0 1-0.3171 0.70466 1.3424 1.3424 0 0 1-0.73286 0.31006 3.238 3.238 0 0 1-1.205-0.049271 3.4317 3.4317 0 0 1-1.1134-0.39108 1.3001 1.3001 0 0 1-0.5602-0.5743 1.2578 1.2578 0 0 1-0.052867-0.75048l0.4827-2.3959 0.90902 0.17264-0.50736 2.3959c-0.063387 0.32415 0.14094 0.48621 0.3312 0.63067a1.7053 1.7053 0 0 0 0.70467 0.25017 1.737 1.737 0 0 0 0.85265-0.024636c0.21493-0.066982 0.31711-0.13741 0.38052-0.44395l0.47213-2.3465z"
-            style="fill: #fff; stroke-width: 0.35233"
-          />
-          <text
-            x="13.664819"
-            y="7.8902383"
-            style="
-              fill: #fff;
-              font-family: FranklinGothic-Medium, 'Franklin Gothic Medium';
-              font-size: 7.4038px;
-              font-weight: 500;
-              stroke-width: 0.33978;
-            "
-            xml:space="preserve"
-          >
-            <tspan style="stroke-width: 0.33978" xml:space="preserve">
-              CKE
-            </tspan>
-          </text>
-        </g>
+        <text
+          x="0"
+          y="7.2"
+          style="fill: #fff; font-size: 7.5px; font-family: 'Franklin Gothic'"
+        >
+          BR
+        </text>
+        <path
+          d="m10.688 2.5585-1.1134-0.056329a0.51088 0.51088 0 0 1-0.52145-0.47213v-0.67295a0.47213 0.47213 0 0 1 0.53554-0.41576l1.0922 0.056329a0.51088 0.51088 0 0 1 0.51794 0.46861v0.70466a0.44746 0.44746 0 0 1-0.51089 0.38758z"
+          style="fill: none; stroke-width: 0.26425px; stroke: #fff"
+        />
+        <path
+          d="m11.135 1.7799a1.4798 1.4798 0 0 1 1.0218 0.028231c0.1092 0.042347 0.45451 0.18674 0.49326 0.29596 0.03875 0.1092-0.06698 0.20788-0.1092 0.28892a0.42985 0.42985 0 0 0 0.27483 0.5743l1.0218 0.42633a0.53555 0.53555 0 0 0 0.68353-0.20083l0.303-0.60954a0.45099 0.45099 0 0 0-0.28539-0.59897l-1.0006-0.41576a0.53202 0.53202 0 0 0-0.68353 0.1973l-0.17616 0.35234"
+          style="fill: none; stroke-width: 0.26425px; stroke: #fff"
+        />
+        <path
+          d="m13.453 3.8058-0.47212 2.3466a1.3706 1.3706 0 0 1-0.3171 0.70466 1.3424 1.3424 0 0 1-0.73286 0.31006 3.238 3.238 0 0 1-1.205-0.049271 3.4317 3.4317 0 0 1-1.1134-0.39108 1.3001 1.3001 0 0 1-0.5602-0.5743 1.2578 1.2578 0 0 1-0.052867-0.75048l0.4827-2.3959 0.90902 0.17264-0.50736 2.3959c-0.063387 0.32415 0.14094 0.48621 0.3312 0.63067a1.7053 1.7053 0 0 0 0.70467 0.25017 1.737 1.737 0 0 0 0.85265-0.024636c0.21493-0.066982 0.31711-0.13741 0.38052-0.44395l0.47213-2.3465z"
+          style="fill: #fff; stroke-width: 0.35233"
+        />
+        <text
+          x="13.8"
+          y="7.2"
+          style="fill: #fff; font-size: 7.5px; font-family: 'Franklin Gothic'"
+        >
+          CKE
+        </text>
       </svg>
       <svg
-        v-if="width <= 300"
+        v-if="width <= 640"
+        class="py-2"
         width="47.747"
         height="50.292"
-        data-name="レイヤー 1"
-        version="1.1"
         viewBox="0 0 47.747 50.292"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:cc="http://creativecommons.org/ns#"
-        xmlns:dc="http://purl.org/dc/elements/1.1/"
-        xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
       >
-        <metadata>
-          <rdf:RDF>
-            <cc:Work rdf:about="">
-              <dc:format>image/svg+xml</dc:format>
-              <dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
-              <dc:title />
-            </cc:Work>
-          </rdf:RDF>
-        </metadata>
-        <defs>
-          <style>
-            .cls-1 {
-              font-size: 21.79px;
-              font-family: FranklinGothic-Medium, Franklin Gothic Medium;
-              font-weight: 500;
-            }
-            .cls-1,
-            .cls-3 {
-              fill: #fff;
-            }
-            .cls-2,
-            .cls-4 {
-              fill: none;
-              stroke: #fff;
-              stroke-miterlimit: 10;
-            }
-            .cls-2 {
-              stroke-width: 0.75px;
-            }
-            .cls-4 {
-              stroke-width: 2.08px;
-            }
-          </style>
-        </defs>
         <path
-          class="cls-4"
           d="m13.881 13.777-8.76-0.45c-2.29-0.12-4.12-1.78-4.08-3.7l0.1-5.31c0-1.92 1.92-3.39 4.21-3.27l8.58 0.45c2.29 0.12 4.12 1.77 4.08 3.7l-0.1 5.45c-0.03 1.85-1.84 3.25-4.03 3.13z"
+          style="fill: none; stroke-width: 2px; stroke: #fff"
         />
         <path
-          class="cls-4"
           d="m17.411 7.6666a11.56 11.56 0 0 1 8 0.2c0.86 0.35 3.59 1.47 3.88 2.34 0.23 0.69-0.53 1.63-0.85 2.27-0.83 1.66 0.13 3.69 2.15 4.53l8.05 3.33c2.1 0.88 4.51 0.18 5.37-1.56l2.39-4.79c0.87-1.74-0.14-3.85-2.24-4.73l-7.89-3.27c-2.1-0.87-4.51-0.17-5.37 1.56l-1.34 2.75"
+          style="fill: none; stroke-width: 2px; stroke: #fff"
         />
         <path
-          class="cls-3"
           d="m35.661 23.587-3.73 18.47a10.76 10.76 0 0 1-2.48 5.51c-1.15 1.17-3.07 2-5.78 2.46a26.11 26.11 0 0 1-9.48-0.38 27.7 27.7 0 0 1-8.76-3.09 10.28 10.28 0 0 1-4.38-4.49 9.75 9.75 0 0 1-0.41-5.92l3.8-18.86 7.15 1.37-3.99 18.84c-0.51 2.53 1.09 3.83 2.59 5 1.5 1.17 3.46 1.53 5.65 2a13.92 13.92 0 0 0 6.7-0.19c1.69-0.54 2.49-1.07 3-3.49l3.71-18.52z"
+          style="fill: none; stroke-width: 2px; stroke: #fff"
         />
         <path
-          class="cls-4"
           d="m13.881 13.777-8.76-0.45c-2.29-0.12-4.12-1.78-4.08-3.7l0.1-5.31c0-1.92 1.92-3.39 4.21-3.27l8.58 0.45c2.29 0.12 4.12 1.77 4.08 3.7l-0.1 5.45c-0.03 1.85-1.84 3.25-4.03 3.13z"
+          style="fill: none; stroke-width: 2px; stroke: #fff"
         />
         <path
-          class="cls-4"
-          d="m17.411 7.6666a11.56 11.56 0 0 1 8 0.2c0.86 0.35 3.59 1.47 3.88 2.34 0.23 0.69-0.53 1.63-0.85 2.27-0.83 1.66 0.13 3.69 2.15 4.53l8.05 3.33c2.1 0.88 4.51 0.18 5.37-1.56l2.39-4.79c0.87-1.74-0.14-3.85-2.24-4.73l-7.89-3.27c-2.1-0.87-4.51-0.17-5.37 1.56l-1.34 2.75"
-        />
-        <path
-          class="cls-3"
           d="m35.661 23.587-3.73 18.47a10.76 10.76 0 0 1-2.48 5.51c-1.15 1.17-3.07 2-5.78 2.46a26.11 26.11 0 0 1-9.48-0.38 27.7 27.7 0 0 1-8.76-3.09 10.28 10.28 0 0 1-4.38-4.49 9.75 9.75 0 0 1-0.41-5.92l3.8-18.86 7.15 1.37-3.99 18.84c-0.51 2.53 1.09 3.83 2.59 5 1.5 1.17 3.46 1.53 5.65 2a13.92 13.92 0 0 0 6.7-0.19c1.69-0.54 2.49-1.07 3-3.49l3.71-18.52z"
+          style="fill: #fff; stroke-width: 0.26425px; stroke: #fff"
         />
       </svg>
     </a>
@@ -218,11 +132,12 @@ import Vue from "vue";
 export default Vue.extend({
   data() {
     return {
-      width: 0,
+      width: 1650,
     };
   },
   mounted() {
     window.addEventListener("resize", this.changeWidth);
+    this.width = window.innerWidth;
   },
   destroyed() {
     window.removeEventListener("resize", this.changeWidth);
